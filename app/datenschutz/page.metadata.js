23ca7b3d3c279dafea7e88c2d520804c5e7e0345
Daniel <ladwig.daniel@icloud.com>
2025-03-03 13:59:05 +0100

    Remove 'use client' directive from legal page components

--- app/datenschutz/page.metadata.js
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-  title: 'Datenschutzerklärung - Montamo',
-  description: 'Datenschutzrichtlinien und Informationen zur Datenverarbeitung der Montamo GmbH',
-}; 
--- app/imprint/page.metadata.js
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-  title: 'Impressum - Montamo',
-  description: 'Impressum und rechtliche Informationen der Montamo GmbH',
-}; 
